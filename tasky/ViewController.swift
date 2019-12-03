9a770fb84c42a9835cea063f1525a0ed371a4dd9
tunay alver <tunay.alver@mobillium.com>
2019-12-03 20:49:40 +0300

    Initial Commit

---
+++ tasky/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tasky
+//
+//  Created by tunay alver on 3.12.2019.
+//  Copyright © 2019 tunay alver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
